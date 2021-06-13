012190199fc660b494fea891e8f9c4cd15a3e42f
Stelele <giftmugweni@gmail.com>
2021-06-13 14:27:45 +0200

    Created function to detect a single key press

--- test.m
+++ test.m
@@ -1,18 +1,16 @@
 N = 512;
 fs = 8000;
-f = 1477;
-k = 1:N;
+f1 = 1209;
+f2 = 697;
+k = 0:255;
 
 dt = 1/fs;
 t = 0:dt:1;
 
-sig = sin(2*pi*f*t);
+sig = sin(2*pi*f1*t) + sin(2*pi*f2*t);
 
 x = zeros(1,N);
 
 x(1:N/2) = sig(1:N/2);
 
-
-X = mygoertzel(x, k, N);
-
-plot(k, 20*log(abs(X)))
+val = detectKeyPress(x, fs);
