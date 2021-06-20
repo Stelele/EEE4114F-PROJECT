148264ac2f61afdc211209e2e55747627961801d
Stelele <giftmugweni@gmail.com>
2021-06-20 23:20:39 +0200

    Added extra checks to make digit validation more robust

--- test.m
+++ test.m
@@ -1,5 +1,7 @@
 
 [signal, fs] = audioread("recordings/network/dtmfN2.wav");
+
+plot(signal);
 %{
 f1 = 697;
 f2 = 1336;
@@ -8,12 +10,15 @@ fs = 8000;
 dt = 1/fs;
 t = 0:dt:2;
 
-signal = sin(2*pi*f1*t) + sin(2*pi*f2*t); 
+signal = 1e-3 * sin(2*pi*f1*t) + 1e-3 * sin(2*pi*f2*t); 
 %}
 figure(500);
 spectrogram(signal,128,120,128,fs,'yaxis');
+%values = fft(signal) / length(signal);
 
-%digit = detectKeyPress(signal, fs);
+%plot(abs(values));
+
+%digit = detectKeyPress(signal(800:end), fs);
 
 digits = detectDigits(signal,fs);
 
